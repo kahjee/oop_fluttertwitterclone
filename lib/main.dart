427a162bc3a7ad33310da7df6df6c1bec0ce9ce0
kahjee <kagieangelomaruyama@gmail.com>
2022-04-07 21:07:13 +0800

    widgets n what not

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,42 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:twitterclone/screens/WrapperScreen.dart';
 import 'package:twitterclone/service/Authentication.dart';
 import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  final Future<FirebaseApp> _init = Firebase.initializeApp(); 
+  MyApp({Key? key}) : super(key: key);
   
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
 
-    final Authentication auth = Authentication();
+    return FutureBuilder(
+      
+      future: _init,
+      builder: (context, snapshot) {
+        if (snapshot.hasError){
+          SnackbarError();
+        }
 
-    return StreamProvider<TwitterUserModel?>.value(
-      value: auth.user, 
-      initialData: null,
-      child: const MaterialApp(home: WrapperScreen()),
-      );
-  }
+        if (snapshot.connectionState == ConnectionState.done){
+          return StreamProvider<TwitterUserModel?>.value(
+          value: Authentication().user, 
+          initialData: null,
+          child: const MaterialApp(home: WrapperScreen()),
+          );
+        }
+        
+    return const Text('Loading');
+  });
+}
 }
